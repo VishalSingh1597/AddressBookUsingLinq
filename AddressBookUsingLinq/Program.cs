a4a250e352fbf08b703faa8fa7f27dba3a332f3d
Vishal Singh <VishalSingh1597>
2021-05-26 17:26:45 +0530

    [Vishal]Add:Welcome To AddressBook Using Linq

---
+++ AddressBookUsingLinq/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookUsingLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***************Welcome To AddressBook Using Linq*************");
+            //Creating DataTable for addressbook problem UC1
+            DataTable addressbookcreatetable = new DataTable();
+        }
+    }
+}
